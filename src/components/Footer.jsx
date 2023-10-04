7a5c1dde40bb076cd94cbfc8461aa68e965224ab
UBnkantah <ubongnkantahuwem@gmail.com>
2023-10-04 21:39:09 +0000

    contact & footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,61 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <section class="section-footer" id="section-footer">
+    <div class="container wrapper">
+
+        <div class="footer-container">
+            <div class="text-center">
+                <img src="/assets/logo-joint.png" alt="Segula Global logo" class="footer-logo" />
+            </div>
+
+        {/* Social media icons */}
+        <ul class="footer-social">
+            {/* linkedin */}
+            <li class="social-item">
+                <a href="https://www.linkedin.com/company/segula-global/?viewAsMember=true" class="social-link" target="blank">
+                    <i class="fa-brands fa-linkedin-in"></i>
+                </a>
+            </li>
+             {/* YouTube  */}
+             {/* <li class="social-item">
+                <a href="https://www.instagram.com/segulaglobal/" class="social-link" target="blank">
+                    <i class="fa-brands fa-youtube"></i>
+                </a>
+            </li> */}
+             {/* Instagram  */}
+            <li class="social-item">
+                <a href="https://www.instagram.com/segulaglobal/" class="social-link" target="blank">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+            </li>
+            {/* Twitter  */}
+            <li class="social-item">
+                <a href="https://twitter.com/segulaglobal" class="social-link" target="blank">
+                    <i class="fa-brands fa-x-twitter"></i>
+                </a>
+            </li>
+        </ul>
+
+         {/* Privacy Policy and Terms of use  */}
+        <ul class="policy">
+            <li class="privacy-item">
+                <a href="#" class="privacy-link">Privacy Policy</a>
+            </li>
+            <li class="privacy-item">
+                <a href="#" class="privacy-link">Terms of Use</a>
+            </li>
+        </ul>
+           
+        </div>
+        
+    </div>
+</section> 
+<footer>
+    <p>&copy; 2023 Segula Global. All Rights Reserved.</p>
+</footer>
+    </>
   )
 }
 
--- src/components/LetTalk.jsx
+++ src/components/LetTalk.jsx
@@ -8,48 +8,72 @@ const LetTalk = () => {
 
     // const options ["e" ]
   return (
-    <div>
-        <div>
-            <div>
-                <h2>Let's Talk</h2>
-                <p>We'd love to hear from you. Leave us a message using the contact form or drop us a line</p>
-                <div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div>
-                    <div></div>
-                    <div></div>
-                </div>
+    <section class="section-contact" id="section-contact">
+    <div class="container wrapper">
+         <div class="contact-container">
+
+            <div class="contact-left">
+                <h1 class="heading-main">Let's Talk</h1>
+                <p>We’d love to hear from you. Leave us a message using the contact form, or drop us a line.</p>
+
+                <ul class="contact-details">
+                    <li class="contact-item">
+                        <i class="fa-solid fa-phone"></i>
+                        <div>
+                            <h3>Call us</h3>
+                            <p>+233 24 465 1930</p>
+                        </div>
+                    </li>
+                    <li class="contact-item">
+                        <i class="fa-regular fa-envelope"></i>
+                        <div>
+                            <h3>Email Us</h3>
+                            <p>info@segulaglobal.com</p>
+                        </div>
+                    </li>
+                    <li class="contact-item">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <div>
+                            <h3>Find us</h3>
+                            <p>17 Boundary Road, East Legon, down Kaiser Kitchen jnt.</p>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <div>
-                <h2>Send a message</h2>
-                <form>
-                    <div>
-                        <label>Full Name</label>
-                        <input type='text' />
-                    </div>
-                    <div>
-                        <label>Email</label>
-                        <input type='text' />
-                    </div>
-                    <div>
-                        <label>Phone</label>
-                        <input type='text' />
-                    </div>
-                    <div>
-                        <label>Full Name</label>
-                        <input type='text' />
-                    </div>
-                    <button>Send</button>
-                </form>
-            </div>
-        </div>
+
+            <form class="contact-form" name="Contact-Form" method="POST" data-netlify="true" action="/form-success/">
+                <h2 class="heading-secondary">Send a message</h2>
+                <div class="input-field">
+                    <label for="fname">Full Name</label>
+                    <input type="text" name="fname" id="fname" required />
+                </div>
+                <div class="input-field">
+                    <label for="email">Email Address</label>
+                    <input type="email" name="email" id="email" required />
+                </div>
+                <div class="input-field">
+                    <label for="phone">Phone Number</label>
+                    <input type="text" name="phone" id="phone" required />
+                </div>
+                <div class="input-field">
+                    <select name="Service" id="lang" required>
+                        <option value="No-choice">I want Segula Global to help me with</option>
+                        <option value="Optimize LinkedIn Profile">Optimize my Linkedin profile</option>
+                        <option value="Photography and videography">Photography &amp; Videography</option>
+                        <option value="Logo Design and Branding">Logo Design and Branding</option>
+                        <option value="Podcast">Create my podcast</option>
+                        <option value="Other">Other</option>
+                    </select>
+                </div>      
+                <div class="input-field">
+                    <label for="message">Add a message</label>
+                    <textarea name="message" id="message" cols="30" rows="10" required></textarea>
+                </div>   
+                <input type="submit" value="Send" class="btn btn-blue" />       
+            </form>
+         </div>
     </div>
+</section>
   )
 }
 
