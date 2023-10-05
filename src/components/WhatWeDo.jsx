2ea07b38657db8d9f5774a8a37b4bc404d735235
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-10-05 13:09:25 +0000

    what we do design done

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -3,18 +3,23 @@ import whatAsset from "../assets/who-we-are.png"
 
 const WhatWeDo = () => {
   return (
-    <div>
+    <div className='section-proposition'>
+      <div className="container wrapper">
         <h2>What We Do</h2>
-        <div>
+        <div className='col-1-2'>
             <div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis tempore excepturi commodi beatae tenetur explicabo, vitae ab? Dolorem magnam cum voluptatibus dolores perspiciatis magni consectetur dolor ipsam, aspernatur maiores laborum?</p>
+                <br />
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, ipsam accusamus qui rem, enim odio ullam distinctio laboriosam necessitatibus beatae praesentium fuga illum saepe blanditiis et. Illum incidunt fugiat provident perspiciatis quo animi! Ullam officiis earum possimus quia nulla quidem?</p>
+                <b></b>
                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, ipsam accusamus qui rem, enim odio ullam distinctio laboriosam necessitatibus beatae praesentium fuga illum saepe blanditiis et. Illum incidunt fugiat provident perspiciatis quo animi! Ullam officiis earum possimus quia nulla quidem?</p>
-                 <button>Call to action</button>
+                 <button className='btn btn-primary mt-20'>Call to action</button>
             </div>
             <div>
                 <img src={whatAsset} alt="" />
             </div>
         </div>
+      </div>
     </div>
   )
 }
