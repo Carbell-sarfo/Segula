e84111834cf82e83d0ca51419a9383b7fae97730
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-10-07 16:11:45 +0000

    making the slider work

--- src/components/ClientCard.jsx
+++ src/components/ClientCard.jsx
@@ -3,16 +3,12 @@ import BoyImg from "../assets/boy.png"
 
 const ClientCard = ({cardImg}) => {
   return (
-    <div className="clientCard-top">
-        <div className="card-container">
-            <div className="img-container">
-                <img src={cardImg} alt="boy" />
-            </div>
-            <h3>Full Name</h3>
-            <p>Position and Title</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        </div>
-    </div>
+      <div className="testimonial">
+          <img src={cardImg} alt="boy" className="testimonial__img" />
+          <h3 className="testimonial__name">Full First Name</h3>
+          <p>Position and Title</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
   )
 }
 
