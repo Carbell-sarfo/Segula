b2268d1c186ec397a477311f9307628289d985d5
UBnkantah <ubongnkantahuwem@gmail.com>
2023-10-07 16:30:29 +0000

    added footer & contact in about & careers page

--- src/pages/AboutPg.jsx
+++ src/pages/AboutPg.jsx
@@ -1,10 +1,14 @@
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import LetTalk from '../components/LetTalk'
 
 const AboutPg = () => {
   return (
     <div>
         <Navbar />
         <h1>This is the About Page</h1>
+        <LetTalk />
+        <Footer />
     </div>
   )
 }
--- src/pages/CareersPg.jsx
+++ src/pages/CareersPg.jsx
@@ -1,10 +1,14 @@
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import LetTalk from '../components/LetTalk'
 
 const CareersPg = () => {
   return (
     <div>
         <Navbar />
         <h1>This is the Career Page</h1>
+        <LetTalk />
+        <Footer />
     </div>
   )
 }
