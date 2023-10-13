81355744f0545c0882b7f70daff2c9bc1b1a9224
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-10-13 13:35:24 +0000

    buttons added to services card

--- src/components/OurService/LinkedInOpt.jsx
+++ src/components/OurService/LinkedInOpt.jsx
@@ -9,8 +9,8 @@ const LinkedInOpt = () => {
         </div>
         <div className='text'>
             <h2>Linkedin Optimisation</h2>
-            <p className='mb-20'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+            <p className='mb-20'>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <button className='btn btn-primary'>Schedule a free call</button>
         </div>
     </div>
--- src/components/OurService/WebDesign.jsx
+++ src/components/OurService/WebDesign.jsx
@@ -10,8 +10,8 @@ const WebDesign = () => {
         <div className='text'>
             <h2>Linkedin Optimisation</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <button>Schedule a free call</button>
+            <p className='mb-20'>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <button className='btn btn-primary'>Schedule a free call</button>
         </div>
     </div>
   )
